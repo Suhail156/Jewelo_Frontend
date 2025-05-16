dbc3d736dda1b5f6a3ddb30cee245d9b60c7664c
sanahiravp0 <sanahiravpo@gmail.com>
2025-05-16 17:18:02 +0530

    Merge remote and local changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: './',  // important for deployment on Vercel
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      // Redirect MUI's styled-engine to the styled-components version:
       '@mui/styled-engine': '@mui/styled-engine-sc'
     }
   }
